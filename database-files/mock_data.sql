2293975aabdafa6470e674e3422f2a67de9a8db5
khanhhoang0712 <namkhanh071204@gmail.com>
2025-08-12 19:49:31 -0400

    Add Kai mock data for gameprofiles, map, and weapons

--- database-files/mock_data.sql
+++ database-files/mock_data.sql
@@ -3,5 +3,14 @@ INSERT IGNORE INTO games (gameID, name) VALUES
 (1, 'Valorant'),
 (2, 'CS2');
 
-INSERT IGNORE INTO profiles (profileID, username, isAdmin, isPublic, isPremium, password) VALUES
-(1, 'KaiGhost', 0, 1, 1, 'test123');
+INSERT IGNORE INTO profiles (username, isAdmin, isPublic, isPremium, password) VALUES
+('KaiGhost', 0, 1, 1, 'test123');
+
+INSERT IGNORE INTO gamesProfiles ( gameID, profileID, gameUsername, showOnDashboard) VALUES
+(1, 1, 'KaiGhost', 1);
+
+INSERT IGNORE INTO map (gameID, POIs, Name) VALUES
+  (1, 'A Site,B Site,Mid', 'Ascent');
+
+INSERT IGNORE INTO weapons (gameID, weaponType, name) VALUES
+ (1, 'Rifle',  'Vandal');
