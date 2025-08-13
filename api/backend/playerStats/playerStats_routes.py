9b953995255d736a5dcb8feb403ee0f40cfd2c15
Elisabeth Chen <chen.eli@northeastern.edu>
2025-08-13 11:11:03 -0400

    routes updates for maps, playerStats, weapons

---
+++ api/backend/playerStats/playerStats_routes.py
@@ -0,0 +1,85 @@
+from flask import Blueprint, request, jsonify, make_response, current_app
+from backend.db_connection import db
+
+playerStats = Blueprint('playerStats', __name__)
+
+# GET stats for a WEAPON used by a specific player in a specific game
+@playerStats.route('/playerStats/<profileID>/<gameID>/<weaponID>', methods=['GET'])
+def get_player_weapon_stats(profileID, gameID, weaponID):
+    current_app.logger.info(f'GET /playerStats/{profileID}/{gameID}/{weaponID}')
+    cursor = db.get_db().cursor()
+    cursor.execute('''SELECT * FROM weaponStats 
+                      WHERE profile_id = %s AND game_id = %s AND weapon_id = %s''',
+                   (profileID, gameID, weaponID))
+    data = cursor.fetchall()
+    response = make_response(jsonify(data))
+    response.status_code = 200
+    return response
+
+# GET stats for a MAP by a specific player in a specific game
+@playerStats.route('/playerStats/<profileID>/<gameID>/<mapID>', methods=['GET'])
+def get_player_map_stats(profileID, gameID, mapID):
+    current_app.logger.info(f'GET /playerStats/{profileID}/{gameID}/{mapID}')
+    cursor = db.get_db().cursor()
+    cursor.execute('''SELECT * FROM mapStats
+                      WHERE profile_id = %s AND game_id = %s AND map_id = %s''',
+                   (profileID, gameID, mapID))
+    data = cursor.fetchall()
+    response = make_response(jsonify(data))
+    response.status_code = 200
+    return response
+
+# POST create stats entry
+@playerStats.route('/playerStats/<profileID>/<gameID>/<weaponID>', methods=['POST'])
+def create_player_weapon_stats(profileID, gameID, weaponID):
+    current_app.logger.info(f'POST /playerStats/{profileID}/{gameID}/{weaponID}')
+    stats_info = request.json
+    kills = stats_info.get('kills', 0)
+    deaths = stats_info.get('deaths', 0)
+    accuracy = stats_info.get('accuracy', None)
+    query = '''INSERT INTO weaponStats (profile_id, game_id, weapon_id, kills, deaths, accuracy)
+               VALUES (%s, %s, %s, %s, %s, %s)'''
+    cursor = db.get_db().cursor()
+    cursor.execute(query, (profileID, gameID, weaponID, kills, deaths, accuracy))
+    db.get_db().commit()
+    return 'Player weapon stats created!', 201
+
+# PUT update stats
+@playerStats.route('/playerStats/<profileID>/<gameID>/<weaponID>', methods=['PUT'])
+def update_player_weapon_stats(profileID, gameID, weaponID):
+    current_app.logger.info(f'PUT /playerStats/{profileID}/{gameID}/{weaponID}')
+    stats_info = request.json
+    kills = stats_info.get('kills')
+    deaths = stats_info.get('deaths')
+    accuracy = stats_info.get('accuracy')
+    query = '''UPDATE weaponStats 
+               SET kills = %s, deaths = %s, accuracy = %s
+               WHERE profile_id = %s AND game_id = %s AND weapon_id = %s'''
+    cursor = db.get_db().cursor()
+    cursor.execute(query, (kills, deaths, accuracy, profileID, gameID, weaponID))
+    db.get_db().commit()
+    return 'Player weapon stats updated!', 200
+
+# DELETE stats record
+@playerStats.route('/playerStats/<profileID>/<gameID>/<weaponID>', methods=['DELETE'])
+def delete_player_weapon_stats(profileID, gameID, weaponID):
+    current_app.logger.info(f'DELETE /playerStats/{profileID}/{gameID}/{weaponID}')
+    cursor = db.get_db().cursor()
+    cursor.execute('''DELETE FROM weaponStats
+                      WHERE profile_id = %s AND game_id = %s AND weapon_id = %s''',
+                   (profileID, gameID, weaponID))
+    db.get_db().commit()
+    return 'Player weapon stats deleted!', 200
+
+# GET all weapons stats for a player in a game
+@playerStats.route('/playerStats/<profileID>/<gameID>', methods=['GET'])
+def get_all_weapon_stats_for_player(profileID, gameID):
+    current_app.logger.info(f'GET /playerStats/{profileID}/{gameID}')
+    cursor = db.get_db().cursor()
+    cursor.execute('''SELECT * FROM weaponStats
+                      WHERE profile_id = %s AND game_id = %s''',
+                   (profileID, gameID))
+    data = cursor.fetchall()
+    response = make_response(jsonify(data))
+    response.status_code = 200
+    return response
---
+++ api/backend/weapons/weapons_routes.py
@@ -0,0 +1,70 @@
+from flask import Blueprint, request, jsonify, make_response, current_app
+from backend.db_connection import db
+
+# Create a new Blueprint 
+weapons = Blueprint('weapons', __name__)
+
+#------------------------------------------------------------
+# Get names of all weapons in a specific game
+@weapons.route('/weapons/<gameID>/', methods=['GET'])
+def get_weapons_by_game(gameID):
+    current_app.logger.info(f'GET /weapons/{gameID} route')
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT weapon_id, name FROM weapons WHERE game_id = %s', (gameID,))
+    theData = cursor.fetchall()
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
+
+# Get information about a specific weapon in a specific game
+@weapons.route('/weapons/<gameID>/<weaponID>', methods=['GET'])
+def get_weapon(gameID, weaponID):
+    current_app.logger.info(f'GET /weapons/{gameID}/{weaponID} route')
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT weapon_id, name, type FROM weapons WHERE game_id = %s AND weapon_id = %s', (gameID, weaponID))
+    theData = cursor.fetchall()
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
+
+#------------------------------------------------------------
+# Create a new weapon in a specific game
+@weapons.route('/weapons/<gameID>/<weaponID>', methods=['POST'])
+def create_weapon(gameID, weaponID):
+    current_app.logger.info(f'POST /weapons/{gameID}/{weaponID} route')
+    weapon_info = request.json
+    name = weapon_info['name']
+    w_type = weapon_info['type']
+    query = 'INSERT INTO weapons (weapon_id, game_id, name, type) VALUES (%s, %s, %s, %s)'
+    data = (weaponID, gameID, name, w_type)
+    cursor = db.get_db().cursor()
+    cursor.execute(query, data)
+    db.get_db().commit()
+    return 'Weapon created!', 201
+
+#------------------------------------------------------------
+# Modify the name or type of a specific weapon
+@weapons.route('/weapons/<gameID>/<weaponID>', methods=['PUT'])
+def update_weapon(gameID, weaponID):
+    current_app.logger.info(f'PUT /weapons/{gameID}/{weaponID} route')
+    weapon_info = request.json
+    name = weapon_info.get('name')
+    w_type = weapon_info.get('type')
+    query = 'UPDATE weapons SET name = %s, type = %s WHERE game_id = %s AND weapon_id = %s'
+    data = (name, w_type, gameID, weaponID)
+    cursor = db.get_db().cursor()
+    cursor.execute(query, data)
+    db.get_db().commit()
+    return 'Weapon updated!'
+
+#------------------------------------------------------------
+# Remove a weapon in a specific game
+@weapons.route('/weapons/<gameID>/<weaponID>', methods=['DELETE'])
+def delete_weapon(gameID, weaponID):
+    current_app.logger.info(f'DELETE /weapons/{gameID}/{weaponID} route')
+    query = 'DELETE FROM weapons WHERE game_id = %s AND weapon_id = %s'
+    data = (gameID, weaponID)
+    cursor = db.get_db().cursor()
+    cursor.execute(query, data)
+    db.get_db().commit()
+    return 'Weapon deleted!'
