58c87c22ea822408979319f64135fb6a78618d96
khanhhoang0712 <namkhanh071204@gmail.com>
2025-08-12 20:30:11 -0400

    add Kai mock data for matches, matchStats, playerStats, weaponStats

--- database-files/mock_data.sql
+++ database-files/mock_data.sql
@@ -14,3 +14,22 @@ INSERT IGNORE INTO map (gameID, POIs, Name) VALUES
 
 INSERT IGNORE INTO weapons (gameID, weaponType, name) VALUES
  (1, 'Rifle',  'Vandal');
+
+INSERT IGNORE INTO matches (gameID, mapID, matchDate, matchType, lobbyRank)
+VALUES
+(1, 1, '2025-08-01 18:30:00','Ranked', 'Immortal');
+
+INSERT IGNORE INTO matchStats (matchID, gameInstanceID, kills, deaths, assists, Headshots, TotalShots,shotsHit, matchDuration, rounds, win, damageDealt,firstBloods)
+VALUES
+(1, 1, 22, 15, 5, 12, 300, 129, 45, 16, 1, 10370, 2); 
+
+INSERT INTO playerStats
+(gameInstanceID, kills, deaths, assists, totalDamage, totalHeadshots, totalShotsHit, totalWins)
+VALUES
+(1, 258, 191, 72, 33850, 132, 1082, 34);
+
+INSERT IGNORE INTO weaponStats
+(statTableID, weaponID, totalUsageTime, kills, accuracy, amountBought)
+VALUES
+(1, 1, 100.000, 180, 0.520, 150),
+(2, 2, 300.000,  78, 0.470,  46);
